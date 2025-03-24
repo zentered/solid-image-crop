56f4e55d11ad1956eb178adfb80b19c556b5706d
Patrick Heneise <patrick@zentered.co>
2025-03-23 20:27:24 -0600

    fix: add cropper selection and canvas settings

--- src/ImageDrop.jsx
+++ src/ImageDrop.jsx
@@ -87,13 +87,37 @@ export default function ImageDrop(props) {
         <div>
           <div>
             <cropper-canvas background class="">
-              <cropper-image ref={cropperImage} alt="cropper" />
+              <cropper-image
+                ref={cropperImage}
+                rotatable
+                scalable
+                skewable
+                translatable
+                alt="cropper"
+              />
               <cropper-handle action="select" plain />
               <cropper-selection
-                ref={cropperSelection}
-                class="w-full h-full"
-                outlined
-              />
+                initial-coverage="0.5"
+                dynamic
+                movable
+                resizable
+                zoomable
+              >
+                <cropper-grid role="grid" covered></cropper-grid>
+                <cropper-crosshair centered></cropper-crosshair>
+                <cropper-handle
+                  action="select"
+                  theme-color="rgba(255, 255, 255, 0.35)"
+                ></cropper-handle>
+                <cropper-handle action="n-resize"></cropper-handle>
+                <cropper-handle action="e-resize"></cropper-handle>
+                <cropper-handle action="s-resize"></cropper-handle>
+                <cropper-handle action="w-resize"></cropper-handle>
+                <cropper-handle action="ne-resize"></cropper-handle>
+                <cropper-handle action="nw-resize"></cropper-handle>
+                <cropper-handle action="se-resize"></cropper-handle>
+                <cropper-handle action="sw-resize"></cropper-handle>
+              </cropper-selection>
             </cropper-canvas>
           </div>
           <button
